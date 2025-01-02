569596267794a3721cd48004c03bca9dc20d6320
Immanuel Olaoye <olaoye939@gmail.com>
2025-01-02 11:54:24 -0600

    added about route to app.jsx file

--- react-personal/src/App.jsx
+++ react-personal/src/App.jsx
@@ -1,12 +1,14 @@
 import { Route, Routes } from 'react-router-dom'
 import Projects from './components/About'
 import Home from './components/Home'
+import About from './components/About'
 
 export default function App() {
   return (
     <Routes>
       <Route path='/' element={<Home />} />
       <Route path='/projects' element={<Projects />} />
+      <Route path='/projects' element={<About />} />
     </Routes>
   )
 }
