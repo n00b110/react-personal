859aae98f7444e95e25d26d2e71b4994401e302e
Immanuel Olaoye <olaoye939@gmail.com>
2025-01-06 23:33:12 -0600

    refactored ui

--- react-personal/src/components/Projects.jsx
+++ react-personal/src/components/Projects.jsx
@@ -10,44 +10,7 @@ const Projects = () => {
             <p class="text-white text-center mt-2">Here are some of the projects I’ve worked on recently.</p>
 
             <div class="mt-10 grid gap-8 sm:grid-cols-2 lg:grid-cols-3">
-            <div class="bg-white rounded-lg shadow-lg overflow-hidden">
-                <div class="p-6">
-                <h3 class="text-xl font-semibold text-gray-800">Finance Web App</h3>
-                <img src="https://placehold.co/393x393" alt="" />
-                <p class="mt-2 text-gray-800">
-                    Built using Python and Bootstrap.
-                </p>
-                <div class="mt-4 flex space-x-4">
-                    <a href="#" class="text-indigo-500 hover:underline">Live Demo</a>
-                    <a href="#" class="text-indigo-500 hover:underline">GitHub</a>
-                </div>
-                </div>
-            </div>
-            <div class="bg-white rounded-lg shadow-lg overflow-hidden">
-                <div class="p-6">
-                <h3 class="text-xl font-semibold text-gray-800">Finance Web App</h3>
-                <p class="mt-2 text-gray-800">
-                    Built using Python and Bootstrap.
-                </p>
-                <div class="mt-4 flex space-x-4">
-                    <a href="#" class="text-indigo-500 hover:underline">Live Demo</a>
-                    <a href="#" class="text-indigo-500 hover:underline">GitHub</a>
-                </div>
-                </div>
-            </div>
-            <div class="bg-white rounded-lg shadow-lg overflow-hidden">
-                <div class="p-6">
-                <h3 class="text-xl font-semibold text-gray-800">Finance Web App</h3>
-                <p class="mt-2 text-gray-800">
-                    Built using Python and Bootstrap.
-                </p>
-                <div class="mt-4 flex space-x-4">
-                    <a href="#" class="text-indigo-500 hover:underline">Live Demo</a>
-                    <a href="#" class="text-indigo-500 hover:underline">GitHub</a>
-                </div>
-                </div>
-            </div>
-            <div class="bg-white rounded-lg shadow-lg overflow-hidden">
+            <div class="bg-white rounded-lg shadow-lg overflow-hidden transform transition-transform duration-300 group-hover:-translate-y-2">
                 <div class="p-6">
                 <h3 class="text-xl font-semibold text-gray-800">Finance Web App</h3>
                 <img src={webScreenshot} alt="" className="m-auto"/>
