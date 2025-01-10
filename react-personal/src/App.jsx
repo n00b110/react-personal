756098726b0524c697a157b2e0fa99d44582bca5
Immanuel Olaoye <olaoye939@gmail.com>
2025-01-09 23:38:31 -0600

    edited Blog component

--- react-personal/src/App.jsx
+++ react-personal/src/App.jsx
@@ -2,6 +2,7 @@ import { Route, Routes } from 'react-router-dom'
 import About from './components/About'
 import Home from './components/Home'
 import Projects from './components/Projects'
+import Blog
 
 export default function App() {
   return (
@@ -9,6 +10,7 @@ export default function App() {
       <Route path='/' element={<Home />} />
       <Route path='/projects' element={<Projects />} />
       <Route path='/about' element={<About />} />
+      <Route path='/blog' element{Blog}></Route>
     </Routes>
   )
 }
---
+++ react-personal/src/components/Blog.jsx
@@ -0,0 +1,7 @@
+const Blog = () => {
+    return (
+        <h1>Coming Soon</h1>
+    )
+}
+
+export default Blog;
