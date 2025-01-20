186b2f057a8176bcdb29ab0ba240c22da4cc6c10
Immanuel Olaoye <olaoye939@gmail.com>
2025-01-19 22:42:25 -0600

    fixed empty components Form.jsx, Blog.jsx, etc.

--- react-personal/src/components/About.jsx
+++ react-personal/src/components/About.jsx
@@ -1,5 +1,11 @@
 const About = () => {
 
+    return (
+
+        <div className="text-4xl bg-gradient-to-l from-cyan-700 to-slate-500 text-white h-screen flex items-center justify-center">
+            <h1 className="text-4xl animate-bounce">Coming Soon</h1>
+        </div>
+    )
 }
 
 
