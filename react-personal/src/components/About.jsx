b118c23797c0cf1ca073dc4cc2ec30286f5c6fbe
Immanuel Olaoye <olaoye939@gmail.com>
2025-01-23 23:06:20 -0600

    fixed Projects.jsx ui

--- react-personal/src/components/About.jsx
+++ react-personal/src/components/About.jsx
@@ -11,7 +11,7 @@
 const About = () => {
 
     return (
-        <div className="text-4xl bg-gradient-to-l from-cyan-700 to-slate-500 text-white">
+        <div className="bg-gradient-to-l from-cyan-700 to-slate-5https://www.linkedin.com/in/immanuel-olaoye/00 text-white h-screen w-screen flex flex-col">
             <h3 className="text-white">My name is Immanuel</h3>
         </div>
     )
