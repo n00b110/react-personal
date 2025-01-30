32410a05e9a0d13bb6855f979be72834d57db891
Immanuel Olaoye <olaoye939@gmail.com>
2025-01-30 17:28:00 -0600

    changed About.jsx

--- react-personal/src/components/About.jsx
+++ react-personal/src/components/About.jsx
@@ -11,8 +11,8 @@
 const About = () => {
 
     return (
-        <div className="bg-gradient-to-l from-cyan-700 to-slate-5https://www.linkedin.com/in/immanuel-olaoye/00 text-white h-screen w-screen flex flex-col">
-            <h3 className="text-white">My name is Immanuel</h3>
+        <div className="bg-gradient-to-l from-cyan-700 to-slate-5 text-white h-screen w-screen flex flex-col">
+            <h3 className="text-white">My name is Not Immanuel</h3>
         </div>
     )
 }
