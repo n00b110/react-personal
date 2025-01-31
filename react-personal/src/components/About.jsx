8d78bf611fcb86b219f55577b82f394758fac039
Immanuel Olaoye <olaoye939@gmail.com>
2025-01-30 22:57:32 -0600

    edited About.jsx

--- react-personal/src/components/About.jsx
+++ react-personal/src/components/About.jsx
@@ -11,8 +11,8 @@
 const About = () => {
 
     return (
-        <div className="bg-gradient-to-l from-cyan-700 to-slate-5 text-white h-screen w-screen flex flex-col">
-            <h3 className="text-white">My name is Not Immanuel</h3>
+        <div className="bg-gradient-to-l from-cyan-700 to-slate-500 text-white h-screen w-screen flex flex-col">
+            <h1>My name is Immanuel</h1>
         </div>
     )
 }
