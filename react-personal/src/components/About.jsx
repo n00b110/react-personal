3ce29a98c3941d77d240887eaec3a55c8dbf4426
Immanuel Olaoye <olaoye939@gmail.com>
2025-02-04 22:55:38 -0600

    fixed positioning error in About.jsx

--- react-personal/src/components/About.jsx
+++ react-personal/src/components/About.jsx
@@ -11,7 +11,7 @@
 const About = () => {
 
     return (
-        <div className="bg-gradient-to-l from-cyan-700 to-slate-500 text-white h-screen w-screen flex flex-col relative top-10 left-10">
+        <div className="bg-gradient-to-l from-cyan-700 to-slate-500 text-white h-screen w-screen flex flex-col relative">
             <h1 className="text-4xl font-bold">About</h1>
             <p className="text-lg animate-fade-in" >My name is Immanuel Olaoye. I'm a student at the University of
                 Missouri-Kansas City. I'm currently pursuing my Master's degree. 
