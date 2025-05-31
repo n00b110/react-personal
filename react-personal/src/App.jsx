e38417ae9322e63e424c1908969579b834eb5555
Immanuel Olaoye <olaoye939@gmail.com>
2025-05-31 12:03:12 -0500

    added vercel user analytics

--- react-personal/src/App.jsx
+++ react-personal/src/App.jsx
@@ -1,20 +1,22 @@
-import { Route, Routes } from 'react-router-dom'
-import About from './components/About'
-import Home from './components/Home'
-import Projects from './components/Projects'
-import Blog from './components/Blog'
-import Form from './components/Form'
+import { Route, Routes } from "react-router-dom";
+import About from "./components/About";
+import Home from "./components/Home";
+import Projects from "./components/Projects";
+import Blog from "./components/Blog";
+import Form from "./components/Form";
+import { Analytics } from "@vercel/analytics/react";
 
 export default function App() {
   return (
-    <Routes>
-      <Route path='/' element={<Home />} />
-      <Route path='/projects' element={<Projects />} />
-      <Route path='/about' element={<About />} />
-      <Route path='/blog' element={<Blog />}></Route>
-      <Route path='/contact' element={<Form />} />
-    </Routes>
-  )
+    <>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/projects" element={<Projects />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/blog" element={<Blog />}></Route>
+        <Route path="/contact" element={<Form />} />
+      </Routes>
+      <Analytics />
+    </>
+  );
 }
-
-
